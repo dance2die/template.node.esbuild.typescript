7854f5c1a632007e0d8501954f8a3e151e25145a
dance2die <sungmkim80@gmail.com>
2021-11-21 13:52:34 -0500

    initial blank BST

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello World3');
+export class BST {
+  value: number;
+  left: BST | null;
+  right: BST | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value: number) {
+    return this;
+  }
+  contains(value: number) {
+    return this;
+  }
+  remove(value: number) {
+    return this;
+  }
+}
